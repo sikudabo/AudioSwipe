0582e7c828eb24f167b421b0283689b558c6c018
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Pro.local>
2023-05-12 21:23:06 -0400

    Adding tsc complilation for the uploads helper from multer

---
+++ db/uploads.js
@@ -0,0 +1,31 @@
+var dotenv = require('dotenv').config();
+var Grid = require('gridfs-stream');
+var GridFsStorage = require('multer-gridfs-storage');
+var mongoose = require('mongoose');
+var multer = require('multer');
+var path = require('path');
+var dbUri = dotenv.parsed.DB_URI;
+var conn = mongoose.createConnection(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+var gfs;
+conn.once('open', function () {
+    // Init Stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+    return 'done';
+});
+var storage = new GridFsStorage({
+    url: dbUri,
+    file: function (req, file) {
+        return new Promise(function (resolve, reject) {
+            var filename = Date.now() + "-" + file.fieldname + path.extname(file.originalname);
+            var fileInfo = {
+                filename: filename,
+                bucketName: 'uploads'
+            };
+            resolve(fileInfo);
+        });
+    }
+});
+var uploads = multer({ storage: storage });
+module.exports = uploads;

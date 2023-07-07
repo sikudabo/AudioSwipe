35c3ac9a8df03546321da4879a614f71672b9e7a
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-07-07 18:39:16 -0400

    Adding new apple-touch-icon images

--- db/schemas/index.js
+++ db/schemas/index.js
@@ -2,8 +2,6 @@ const artistSchema = require('./artistSchema');
 const fanSchema = require('./fanSchema');
 const songSchema = require('./songSchema');
 
-console.log('Update');
-
 module.exports = {
     artistSchema,
     fanSchema,

80e6344afdf5853f33c396c084fe2b08ac7fc327
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-08-07 16:08:52 -0400

    Song fetching updates

--- routes/fetchGenre.js
+++ routes/fetchGenre.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { SongModel } = require('../db/models');
+const { FanModel, SongModel } = require('../db/models');
 
-router.route('/api/fetch-genre/:genre').get(async (req, res) => {
-    const { genre } = req.params;
+router.route('/api/fetch-genre/:genre/:fanId').get(async (req, res) => {
+    const { fanId, genre } = req.params;
     
     try {
         const songs = await SongModel.find({ genres: genre });
-        res.status(200).json({ audioClips: songs, isSuccess: true });
+        const fan = await FanModel.find({ _id: fanId }, { songsHeard: 1, _id: 0 });
+        console.log('The fan is:', fan);
+        let filteredSongs = songs;
+
+        if (typeof songs !== 'undefined' && songs.length > 0 && typeof fan.songsHeader !== 'undefined') {
+            filteredSongs = songs.filter((songId) => !fan.songsHeard.includes(songId));
+        }
+
+        res.status(200).json({ audioClips: filteredSongs, isSuccess: true });
     } catch(e) {
         console.log('There was an retrieving a genre of audio clips');
         console.log(e.message);

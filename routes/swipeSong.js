5bddf677481c46acda6a84078b4defb78bd9f9d9
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-08-21 21:43:44 -0400

    Storing likes and dislikes for individual songs

--- routes/swipeSong.js
+++ routes/swipeSong.js
@@ -2,21 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Grid = require('gridfs-stream');
 const mongoose = require('mongoose');
-const { FanModel } = require('../db/models');
+const { FanModel, SongModel } = require('../db/models');
 
 router.route('/api/swipe').post(async (req, res) => {
     console.log('I am being hit!');
-    const { direction, fanId, songId } = req.body;
+    const { direction, fanId, gender, songId } = req.body;
+    const date = new Date();
+    const month = date.getMonth();
+    const year = date.getFullYear();
 
     try {
 
         if (direction === 'right') {
             await FanModel.updateOne({ _id: fanId }, { $push: { likedSongs: songId }});
+            await SongModel.updateOne({ _id: songId }, { $push: { likes: { createOn: new Date().getTime(), fanId, gender, month, year  }}});
             res.status(200).json({ isSuccess: true, message: 'Successfully liked song' });
             return;
         }
 
         await FanModel.updateOne({ _id: fanId }, { $push: { dislikedSongs: songId }});
+        await SongModel.updateOne({ _id: songId }, { $push: { likes: { createOn: new Date().getTime(), fanId, gender, month, year  }}});
         res.status(200).json({ isSuccess: true, message: 'Successfully disliked song' });
     } catch(e) {
         console.log('Error swiping on a song!!!!!!!!!!!!!');

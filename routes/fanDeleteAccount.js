b5515ffca6413a0535d70ef6888ca519247ef7b0
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-08-28 16:11:52 -0400

    Adding endpoint to delete a fan

--- routes/fanDeleteAccount.js
+++ routes/fanDeleteAccount.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Grid = require('gridfs-stream');
+const GridFsStorage = require('multer-gridfs-storage').GridFsStorage;
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 const { FanModel } = require('../db/models');
+
+const dbUri = process.env.DB_URI;
+
+var conn = mongoose.createConnection(dbUri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let gfs;
+
+conn.once('open', async () => {
+    // Init Stream
+    gfs = await Grid(conn.db, mongoose.mongo);
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'uploads',
+    });
+    await gfs.collection('uploads');
+    return 'done';
+});
+
+const storage = new GridFsStorage({
+    url: dbUri,
+    file: async (req, file) => {
+      return await new Promise((resolve, reject) => {
+          const filename = Date.now() + "-" + file.fieldname + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+    }
+});
+
+const uploads = multer({ storage });
 
 router.route('/api/delete-fan').delete(async (req, res) => {
     const { _id, avatarId } = req.body;

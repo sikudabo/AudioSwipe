dc1a10f7da3c76b3b2d0bb4ff807fc49f91c40f5
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-08-28 16:37:55 -0400

    Adding songs to the fetch artist response.

--- routes/fetchArtist.js
+++ routes/fetchArtist.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { ArtistModel } = require('../db/models');
+const { ArtistModel, SongModel } = require('../db/models');
 
 router.route('/api/fetch-artist/:artistId').get(async (req, res) => {
     const { artistId } = req.params;
 
     try {
         const artist = await ArtistModel.findOne({ _id: artistId });
-        res.status(200).json({ isSuccess: true, artist });
+        const artistSongs = await SongModel.find({ artistId }, { _id: 1, album: 1, albumCover: 1, disLikes: 1, likes: 1, name: 1, songMediaId: 1 });
+        res.status(200).json({ isSuccess: true, artist, artistSongs });
     } catch (e) {
         console.log('There was an error fetching an artist!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         console.log('Error message:', e.message);

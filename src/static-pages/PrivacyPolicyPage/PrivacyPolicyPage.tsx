1dbce3b3195c5faaaec5b8591a9426901a27e253
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-02 22:07:04 -0400

    Updating privacy policy

--- src/static-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ src/static-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -48,7 +48,7 @@ export default function PrivacyPolicyPage() {
             <div className="privacy-policy-body-section">
                 <p>1. AudioSwipe allows you to request all of your personal data at any time and it can be sent to you in a digital format. </p>
                 <p>2. The personal data we collect is your first name, last name, gender, email, password, age, phone number and an image avatar.</p> 
-                <p>3. We use this data for verification purposes and for artists to get demographics data feedback. The artists do not know who you are and personal data like your name, email, phone number and avatar are not shared. Today, only the gender of the person who liked their audio clip is shared.</p>
+                <p>3. We use this data for verification purposes, to render you services and for artists to get demographics data feedback. The artists do not know who you are and personal data like your name, email, phone number and avatar are not shared. Today, only the gender of the person who liked their audio clip is shared.</p>
                 <p>4. This data is not shared with third-party sources besides the artists on the platform, and all of the data besides the gender data is obscured from the artist.</p>
                 <p>5. We will not share your personal data with third-party advitisers, marketers, organizations or internet service providers unless we are required to by law.</p>
 

07be084eb485a339a9d4151f7772c04b5f36ed19
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-02 22:05:22 -0400

    Adding the privacy policy.

--- src/static-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
+++ src/static-pages/PrivacyPolicyPage/PrivacyPolicyPage.tsx
@@ -5,7 +5,7 @@ import { colors } from '../../components';
 
 const PrivacyPolicyContainer = styled(Grid)`
     background-color: ${colors.hotPink};
-    height: 100vh;
+    height: 100%;
     overflow: scroll;
     width: 100vw;
 
@@ -14,12 +14,25 @@ const PrivacyPolicyContainer = styled(Grid)`
     }
 
     .privacy-policy-header-container {
-        align-items: center;
         display: flex;
+        justify-content: center;
+        padding-top: 100px;
+        width: 100vw;
 
         .privacy-policy-header-text {
+            align-self: center;
             font-size: 42px;
             font-weight: 900;
+        }
+    }
+
+    .privacy-policy-body-section {
+        padding-left: 10px;
+        padding-right: 10px;
+
+        p {
+            font-size: 28px;
+            font-weight: 700;
         }
     }
 `;
@@ -31,6 +44,14 @@ export default function PrivacyPolicyPage() {
                 <p className="privacy-policy-header-text">
                     Privacy Policy
                 </p>
+            </div>
+            <div className="privacy-policy-body-section">
+                <p>1. AudioSwipe allows you to request all of your personal data at any time and it can be sent to you in a digital format. </p>
+                <p>2. The personal data we collect is your first name, last name, gender, email, password, age, phone number and an image avatar.</p> 
+                <p>3. We use this data for verification purposes and for artists to get demographics data feedback. The artists do not know who you are and personal data like your name, email, phone number and avatar are not shared. Today, only the gender of the person who liked their audio clip is shared.</p>
+                <p>4. This data is not shared with third-party sources besides the artists on the platform, and all of the data besides the gender data is obscured from the artist.</p>
+                <p>5. We will not share your personal data with third-party advitisers, marketers, organizations or internet service providers unless we are required to by law.</p>
+
             </div>
         </PrivacyPolicyContainer>
     );
